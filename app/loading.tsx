673626039ca16b4fd635e9be11d59ea41df1efdd
Vinoth <vinoth.kumar@credo.health>
2023-09-25 23:02:52 +0530

    input css and loader

--- app/loading.tsx
+++ app/loading.tsx
@@ -1,7 +1,98 @@
 import React from 'react'
 
 const Loading = () => {
-    return <div className="text-center">Loading...</div>
+    const theme = '#ff6702'
+    const theme_secondary = '#F7BD97'
+
+    return (
+        <div className="flex justify-center">
+            <div>
+                <svg height="108px" width="108px" viewBox="0 0 128 128" className="loader">
+                    <defs>
+                        <clipPath id="loader-eyes">
+                            <circle
+                                transform="rotate(-40,64,64) translate(0,-56)"
+                                r="8"
+                                cy="64"
+                                cx="64"
+                                className="loader__eye1"
+                            ></circle>
+                            <circle
+                                transform="rotate(40,64,64) translate(0,-56)"
+                                r="8"
+                                cy="64"
+                                cx="64"
+                                className="loader__eye2"
+                            ></circle>
+                        </clipPath>
+                        <linearGradient y2="1" x2="0" y1="0" x1="0" id="loader-grad">
+                            <stop stop-color="#000" offset="0%"></stop>
+                            <stop stop-color="#fff" offset="100%"></stop>
+                        </linearGradient>
+                        <mask id="loader-mask">
+                            <rect
+                                fill="url(#loader-grad)"
+                                height="128"
+                                width="128"
+                                y="0"
+                                x="0"
+                            ></rect>
+                        </mask>
+                    </defs>
+                    <g stroke-dasharray="175.93 351.86" stroke-width="12" stroke-linecap="round">
+                        <g>
+                            <rect
+                                clip-path="url(#loader-eyes)"
+                                height="64"
+                                width="128"
+                                fill={theme_secondary}
+                            ></rect>
+                            <g stroke={theme_secondary} fill="none">
+                                <circle
+                                    transform="rotate(180,64,64)"
+                                    r="56"
+                                    cy="64"
+                                    cx="64"
+                                    className="loader__mouth1"
+                                ></circle>
+                                <circle
+                                    transform="rotate(0,64,64)"
+                                    r="56"
+                                    cy="64"
+                                    cx="64"
+                                    className="loader__mouth2"
+                                ></circle>
+                            </g>
+                        </g>
+                        <g mask="url(#loader-mask)">
+                            <rect
+                                clip-path="url(#loader-eyes)"
+                                height="64"
+                                width="128"
+                                fill={theme}
+                            ></rect>
+                            <g stroke={theme} fill="none">
+                                <circle
+                                    transform="rotate(180,64,64)"
+                                    r="56"
+                                    cy="64"
+                                    cx="64"
+                                    className="loader__mouth1"
+                                ></circle>
+                                <circle
+                                    transform="rotate(0,64,64)"
+                                    r="56"
+                                    cy="64"
+                                    cx="64"
+                                    className="loader__mouth2"
+                                ></circle>
+                            </g>
+                        </g>
+                    </g>
+                </svg>
+            </div>
+        </div>
+    )
 }
 
 export default Loading
--- components/Buttons/Button.tsx
+++ components/Buttons/Button.tsx
@@ -5,7 +5,7 @@ export const button = tv({
     base: 'font-medium',
     variants: {
         color: {
-            primary: 'bg-gradient-to-b from-theme to-theme_secondary text-white',
+            primary: 'bg-gradient-to-b to-theme from-theme_secondary to-60% text-white',
             danger: 'bg-error text-white hover:shadow-lg hover:shadow-danger-200'
         },
         size: {
